327115ab7f134acc562e2b7efae67483684de3f2
Andriy Sydorchuk <programmers130822@gmail.com>
2026-05-02 16:10:52 +0300

    Refactor function invoking structure and remove old modal logic statements

--- src/index.js
+++ src/index.js
@@ -1,20 +1,11 @@
 import "./style.css";
-import { addTaskForm } from "./addtaskform";
-import { addTaskModal } from './modal';
+import { handleTaskForm } from "./addtaskform";
+import { handleModal } from './modal';
 import { handleNavTabs } from './navtabs';
 import { toggleElementByClass } from './domhelper';
 
 document.addEventListener("DOMContentLoaded", () => {
     handleNavTabs();
-
-    const addTaskBtn = document.querySelector(".addtask-btn");
-    addTaskBtn.addEventListener("click", () => {
-        toggleElementByClass("empty-state");
-
-        const main = document.querySelector(".main");
-        addTaskForm(main);
-    })
-
-    const sidebarAddTaskBtn = document.querySelector(".sidebar-newtask-btn");
-    sidebarAddTaskBtn.addEventListener("click", () => addTaskModal())
-})
+    handleTaskForm();
+    handleModal();
+});
